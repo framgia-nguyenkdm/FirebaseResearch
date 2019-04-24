6a448f71df130bf9eca60747939ebdb9776e1ef0
Khuc Dinh Minh Nguyen <khuc.dinh.minh.nguyen@framgia.com>
2019-04-24 14:39:08 +0700

    Initial Commit

---
+++ FirebaseResearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseResearch
+//
+//  Created by Khuc Dinh Minh Nguyen on 4/24/19.
+//  Copyright © 2019 Khuc Dinh Minh Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
