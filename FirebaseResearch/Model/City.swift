f0ef44d1d480e4fdf608f8394d9b9b11112810be
Khuc Dinh Minh Nguyen <khuc.dinh.minh.nguyen@framgia.com>
2019-05-07 10:32:05 +0700

    Commit README

---
+++ FirebaseResearch/Model/City.swift
@@ -0,0 +1,9 @@
+//
+//  City.swift
+//  FirebaseResearch
+//
+//  Created by Khuc Dinh Minh Nguyen on 4/25/19.
+//  Copyright © 2019 Khuc Dinh Minh Nguyen. All rights reserved.
+//
+
+import Foundation
