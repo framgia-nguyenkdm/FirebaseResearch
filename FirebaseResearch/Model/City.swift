ce64b9178dae464308f39a89a6ed56461462e162
Khuc Dinh Minh Nguyen <khuc.dinh.minh.nguyen@framgia.com>
2019-05-08 13:12:15 +0700

    Create circleCI config and gitignore

--- FirebaseResearch/Model/City.swift
+++ FirebaseResearch/Model/City.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+
+struct City {
+    var id: String
+    var name: String
+    var image: String
+    var population: Int
+    var isVisited: Bool
+    
+    var dictionary: [String : Any] {
+        return [
+            "id": id,
+            "name": name,
+            "images": image,
+            "population": population,
+            "visited": isVisited
+        ]
+    }
+}
+
+extension City {
+    init?(dictionary: [String: Any], id: String) {
+        guard let id = dictionary["id"] as? String,
+            let name = dictionary["name"] as? String,
+            let image = dictionary["images"] as? String,
+            let population = dictionary["population"] as? Int,
+            let isVisited = dictionary["visited"] as? Bool else {
+                return nil
+        }
+        
+        self.init(id: id, name: name, image: image, population: population, isVisited: isVisited)
+    }
+}
--- FirebaseResearch/ViewController.swift
+++ FirebaseResearch/ViewController.swift
@@ -7,14 +7,127 @@
 //
 
 import UIKit
+import Firebase
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AuthUIDelegate {
+    private var documents: [DocumentSnapshot] = []
+    private var listener: ListenerRegistration!
+    var cities: [City] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        getListCities()
+        observerListCities(limit: 20)
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.listener.remove()
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+        let ref = Firestore.firestore().collection("Cities")
+        
+        ref.document("DNJS123jsns32knd").setData([
+            "id": "1",
+            "images": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHPNKXqZwUOqKrG-TCDxyMxSkUfFO28MyaVSd-AYNWQeoQUx55",
+            "name": "Viet Nam",
+            "population": 98000000,
+            "visited": true
+            ])
+    }
+    @IBAction func reconnectTapped(_ sender: Any) {
+        Firestore.firestore().enableNetwork { (error) in
+        
+        }
+        
+    }
+    @IBAction func disconnectTapped(_ sender: Any) {
+        Firestore.firestore().disableNetwork { (error) in
+            
+        }
+    }
+    
+}
 
 
+extension ViewController {
+    func getListCities() {
+        let database = Firestore.firestore()
+        let query = database.collection("Cities")
+        
+        query.whereField("name", isEqualTo: "VietNam")
+            .getDocuments { (snapshot, error) in
+            guard let snapshot = snapshot, !snapshot.isEmpty else {
+                print("Empty database")
+                return
+            }
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+                return
+            }
+            
+            let data = snapshot.documents.map({ (document) -> City in
+                if let city = City(dictionary: document.data(), id: document.documentID) {
+                    return city
+                } else {
+                    fatalError("Unable to initialize type \(City.self) with dictionary \(document.data())")
+                }
+            })
+            
+            self.cities = data
+            print("Nguyen fetch data: \(self.cities)")
+        }
+    }
+    
+    func observerListCities(limit: Int?) {
+        let db = Firestore.firestore()
+        let query: Query
+        if let limit = limit {
+            query = db.collection("Cities")
+                .limit(to: limit)
+        } else {
+            query = db.collection("Cities")
+        }
+        self.listener = query.addSnapshotListener({ (snapshot, error) in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let snapshot = snapshot else {
+                print("Error fetching document: \(error!)")
+                return
+            }
+            
+            // change data from snapshot
+//            snapshot.documentChanges.forEach({ (diff) in
+//                if (diff.type == .added) {
+//                    print("New city: \(diff.document.data())")
+//                }
+//                if (diff.type == .modified) {
+//                    print("Modified city: \(diff.document.data())")
+//                }
+//                if (diff.type == .removed) {
+//                    print("Removed city: \(diff.document.data())")
+//                }
+//            })
+            
+            
+            let source = snapshot.metadata.hasPendingWrites ? "Local" : "Server"
+            print("\(source) data)")
+            let data = snapshot.documents.map({ (document) -> City in
+                if let city = City(dictionary: document.data(), id: document.documentID) {
+                    return city
+                } else {
+                    fatalError("Unable to initialize type \(City.self) with dictionary \(document.data())")
+                }
+            })
+            
+            self.cities = data
+            self.documents = snapshot.documents
+            print("Nguyen observer data data: \(self.cities)")
+        })
+    }
 }
 
